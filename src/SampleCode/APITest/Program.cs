2c60e71fb3196be92cfde4823cdf60d714831cd1
laptop1 <laptop1@gmail.com>
2020-09-20 01:12:42 -0400

    Updates

--- src/SampleCode/APITest/Program.cs
+++ src/SampleCode/APITest/Program.cs
@@ -13,7 +13,7 @@ namespace OCRAPITest
         [STAThread]
         static void Main()
         {
-            UpdateApplication();
+            Task.Run(() => UpdateApplication());
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
